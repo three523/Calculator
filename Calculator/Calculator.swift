df467dee3f3786b7c4b61e3982f56237ffdcd9cb
dohyun <thtree523@gmail.com>
2023-07-20 22:49:38 +0900

    버그 픽스

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -21,14 +21,16 @@ class Calculator {
     private var exit: Bool = false
 
     func run() {
+        print("*** 주의사항: 입력할떄 괄호()를 꼭 직접 입력하여 닫아주셔야 합니다. 자동완성으로 괄호가 닫히는 경우 입력이 제대로 되지 않습니다.***")
         while !exit {
             input()
         }
         print("종료되었습니다.")
     }
     private func input() {
-        print("계산할 식을 입력해주세요(ex: 12+8*2, +12-12*2)")
-        print("이전 노드 지우기:b 완전히 지우기:c 종료:x")
+        formula.isEmpty ? print("계산할 식을 입력해주세요(ex: +3, 12+3*3, 2+2(3+4)2+3/3)") :
+                           print("계산할 식을 추가로 입력해주세요 (ex: +, 2, +2, (2.2+2), +3(2+3))")
+        print("한글자 지우기:b 모두 지우기:c 종료:x")
         let stringFormula = formula.joined()
         print("현재 계산된 식: \(stringFormula.isEmpty ? "0" : stringFormula)")
         print("현재 결과값: \(result)")
@@ -38,12 +40,33 @@ class Calculator {
         }
     }
     private func validation(input: String) -> Bool {
+        var input = input
+        if let oldLastStr = formula.last,
+           !oldLastStr.isBrackets {
+            input.insert(contentsOf: oldLastStr, at: input.startIndex)
+        }
         let reg: String = "^[0-9+/*().-]{1,}$"
         let reg2: String = "[+/*%.-]{2,}"
         let reg3: String = "\\(\\)"
+        let reg4: String = "\\([+/*%.-]|[+/*%.-]\\)"
         let validation1 = input.range(of: reg, options: .regularExpression) != nil // 0-9+/*().- 이 글자들로만 구성이 되어있는지 확인
         let validation2 = input.range(of: reg2, options: .regularExpression) == nil // +/*%.- 이 연산자가 두번 연속으로 나오지 않는지 확인
         let validation3 = input.range(of: reg3, options: .regularExpression) == nil // 빈 괄호() 가 나오지 않는지 확인
+        let validation4 = input.range(of: reg4, options: .regularExpression) == nil // (+2), (2-) 같은 입력이 나오지 않는지 확인
+        
+        let reg5 = "\\("
+        let reg6 = "\\)"
+        do {
+            let regex1 = try NSRegularExpression(pattern: reg5)
+            let regex2 = try NSRegularExpression(pattern: reg6)
+            let results1 = regex1.matches(in: input,
+                                          range: NSRange(input.startIndex..., in: input)).count
+            let results2 = regex2.matches(in: input,
+                                          range: NSRange(input.startIndex..., in: input)).count
+            return validation1 && validation2 && validation3 && validation4  && results1 == results2
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+        }
         return validation1 && validation2 && validation3
     }
     private func read(input: String) {
@@ -60,22 +83,25 @@ class Calculator {
         }
         guard validation(input: removeSpaceInput) else {
             print("잘못된 입력입니다.")
+            result = 0
             return
         }
-        let inputFormula = inputSplit(input: removeSpaceInput)
-        let newFormula = formulaAppend(newFormula: inputFormula)
+        let newFormula = formulaAppend(appendFormula: removeSpaceInput.map{ String($0) })
         calculate(formula: newFormula)
     }
-    private func formulaAppend(newFormula: [String]) -> [String] {
-        var newFormula = newFormula
+    private func formulaAppend(appendFormula: [String]) -> [String] {
+        var appendFormula = appendFormula
         var oldFormula = formula
+        if let str = appendFormula.first,
+           str.isOperator && oldFormula.isEmpty {
+            appendFormula.insert("0", at: 0)
+        }
         var result = [String]()
         if !oldFormula.isEmpty {
             result.append(oldFormula.popLast()!)
         }
-        
-        while !newFormula.isEmpty {
-            var temp = newFormula.removeFirst()
+        while !appendFormula.isEmpty {
+            let temp = appendFormula.removeFirst()
             if result.isEmpty {
                 result.append(temp)
             } else if let resultLastStr = result.last,
@@ -86,44 +112,26 @@ class Calculator {
                       resultLastStr.isNumber && temp == "(" {
                 result.append("*")
                 result.append(temp)
+            } else if let lastStr = result.last,
+                      lastStr.isNumber && temp.isNumber {
+                result.removeLast()
+                result.append(lastStr + temp)
+            } else if let lastStr = result.last,
+                      let lastChar = lastStr.last,
+                      lastChar == "." && temp.isNumber {
+                result.removeLast()
+                result.append(lastStr + temp)
+            } else if let lastStr = result.last,
+                      lastStr.isNumber && temp == "." {
+                result.removeLast()
+                result.append(lastStr + temp)
             } else {
                 result.append(temp)
             }
         }
         var tempFormula = oldFormula + result
-        if let newFirstFormula = newFormula.first,
-           let oldLastFormula = tempFormula.last,
-           newFirstFormula.isNumber && oldLastFormula.isNumber && !tempFormula.isEmpty {
-            guard let lastNum = tempFormula.popLast() else { return formula }
-            newFormula.removeFirst()
-            tempFormula.append(lastNum + newFirstFormula)
-        } else if let newFirstFormula = newFormula.first,
-                  let oldLastFormula = tempFormula.last,
-                  newFirstFormula.isBrackets && oldLastFormula.isNumber {
-            newFormula.insert("*", at: 0)
-        } else if let newFirstFormula = newFormula.first,
-                  let oldLastFormula = tempFormula.last,
-                  newFirstFormula.isNumber && oldLastFormula.isBrackets {
-            newFormula.insert("*", at: 0)
-        } else if let newFirstFormula = newFormula.first,
-                  let oldLastFormula = tempFormula.last,
-                  newFirstFormula.isBrackets && oldLastFormula.isBrackets {
-            newFormula.insert("*", at: 0)
-        }
-        tempFormula.append(contentsOf: newFormula)
-        guard validation(input: tempFormula.joined()) else {
-            print("입력값이 잘못되었습니다.")
-            return formula
-        }
+        tempFormula.append(contentsOf: appendFormula)
         return tempFormula
-    }
-    private func test(newFormula: [String]) -> Bool {
-        guard let newFirstFormula = newFormula.first,
-              let oldLastFormula = formula.last else { return false }
-        let inputErrorCase1 = oldLastFormula.isNumber && newFirstFormula.isBrackets // ex: 2+2(2+2)
-        let inputErrorCase2 = oldLastFormula.isBrackets && newFirstFormula.isNumber // ex: (2+2)2+3
-        let inputErrorCase3 = oldLastFormula.isBrackets && newFirstFormula.isBrackets // ex: 2+2()
-        return inputErrorCase1 || inputErrorCase2 || inputErrorCase3
     }
     private func calculate(formula: [String]) {
         if let lastText = formula.last,
@@ -143,6 +151,7 @@ class Calculator {
                     let num1 = stack.removeLast()
                     guard let operate = Operator(rawValue: str) else {
                         print("입력값이 잘못되었습니다.")
+                        result = 0
                         return
                     }
                     stack.append(continueOperation(num1: num1, num2: num2, operation: operate))
@@ -150,11 +159,13 @@ class Calculator {
                     break
                 } else {
                     print("입력값이 잘못되었습니다.")
+                    result = 0
                     return
                 }
             } else {
                 guard let num = Double(str) else {
                     print("double로 변환이 불가능합니다.")
+                    result = 0
                     return
                 }
                 stack.append(num)
@@ -167,26 +178,6 @@ class Calculator {
         self.result = result
         self.formula = formula
         print("결과: \(result)\n")
-    }
-    private func inputSplit(input: String) -> [String] {
-        var numJoinInput = [String]()
-        var inputArray = input.map{ String($0) }
-        if let str = inputArray.first,
-           str.isOperator && formula.isEmpty {
-            inputArray.insert("0", at: 0)
-        }
-        var temp = [String]()
-        inputArray.forEach { str in
-            if str.isNumber {
-                temp.append(str)
-            } else {
-                if !temp.isEmpty { numJoinInput.append(temp.joined()) }
-                numJoinInput.append(str)
-                temp.removeAll()
-            }
-        }
-        if !temp.isEmpty { numJoinInput.append(temp.joined()) }
-        return numJoinInput
     }
     private func clear() {
         formula.removeAll()
